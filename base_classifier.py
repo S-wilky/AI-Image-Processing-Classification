79f3d8d299388c2fa35b85fd7ab7759ca615a235
S-Wilky <wilkinsonshane12@gmail.com>
2025-01-26 13:08:15 -0800

    new car image

--- base_classifier.py
+++ base_classifier.py
@@ -31,5 +31,5 @@ def classify_image(image_path):
         print(f"Error processing image: {e}")
 
 if __name__ == "__main__":
-    image_path = "basic_cat.jpg"  
+    image_path = "desert_car.jpg"  
     classify_image(image_path)
